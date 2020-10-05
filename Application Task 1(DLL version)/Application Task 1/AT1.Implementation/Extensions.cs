364aa3e322d67f316ecd9932b538908d2cb7bc34
ABHI AUJLA <asauj@deakin.edu.au>
2020-10-06 03:06:05 +1100

    ENTIRE PROJECT

---
+++ Application Task 1(DLL version)/Application Task 1/AT1.Implementation/Extensions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AT1.Implementation
+{
+	public static class Extensions
+	{
+		// Computes total time used by this processor for its tasks
+		public static double Time ( this Processor processor )
+		{
+			return processor.AllocatedTasks.Sum ( x => x.ElapsedTime ( processor ) );
+		}
+
+		// Extension method to find max element in a sequence by a specific property
+		public static TSource MaxBy<TSource, TKey> ( this IEnumerable<TSource> source,
+													 Func<TSource, TKey> selector )
+		{
+			var comparer = Comparer<TKey>.Default;
+
+			using ( var sourceIterator = source.GetEnumerator ( ) )
+			{
+				if ( !sourceIterator.MoveNext ( ) )
+					throw new InvalidOperationException ( "Sequence contains no elements" );
+
+				var min    = sourceIterator.Current;
+				var minKey = selector ( min );
+				while ( sourceIterator.MoveNext ( ) )
+				{
+					var candidate          = sourceIterator.Current;
+					var candidateProjected = selector ( candidate );
+					if ( comparer.Compare ( candidateProjected, minKey ) > 0 )
+					{
+						min    = candidate;
+						minKey = candidateProjected;
+					}
+				}
+
+				return min;
+			}
+		}
+
+		// Convert allocation to TAFF string
+		public static string ToTaffString ( this Allocation allocation )
+		{
+			var builder = new StringBuilder ( );
+			builder.AppendLine ( $"ALLOCATION-ID={allocation.ID}" );
+
+			foreach ( var processor in allocation.Processors )
+			{
+				var tasks = new int[allocation.NumberOfTasks];
+				foreach ( var task in processor.AllocatedTasks )
+					tasks[task.ID - 1] = 1;
+				builder.AppendLine ( string.Join ( ",", tasks ) );
+			}
+
+			return builder.ToString ( );
+		}
+
+		// Convert allocations to TAFF string
+		public static string ToTaffString ( this Allocations allocations, Configuration config )
+		{
+			var builder = new StringBuilder ( );
+			builder.AppendLine ( $"CONFIG-FILE=\"{config.FilePath}\"" );
+			builder.AppendLine ( );
+			builder.AppendLine (
+				$"ALLOCATIONS-DATA={allocations.Count},{config.NumberOfTasks},{config.NumberOfProcessors}" );
+			builder.AppendLine ( );
+
+			foreach ( var allocation in allocations )
+			{
+				builder.AppendLine ( allocation.ToTaffString ( ) );
+				builder.AppendLine ( );
+			}
+
+			return builder.ToString ( );
+		}
+	}
+}
---
+++ Application Task 1(DLL version)/Application Task 1/Application Task 1/AllocationsViewer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using AT1;
+
+namespace AllocationsApplication
+{
+	partial class AllocationsViewerForm : Form
+	{
+#region properties
+
+		private Allocations   AT1Allocations;
+		private Configuration AT1Configuration;
+		private ErrorsViewer  ErrorListViewer = new ErrorsViewer ( );
+		private AboutBox      AboutBox        = new AboutBox ( );
+
+#endregion
+
+#region constructors
+
+		public AllocationsViewerForm ( )
+		{
+			InitializeComponent ( );
+		}
+
+#endregion
+
+#region File menu event handlers
+
+		private void OpenAllocationsFileToolStripMenuItem_Click ( object sender, EventArgs e )
+		{
+			ClearGUI ( );
+
+			// Process allocations and configuration files.
+			if ( openFileDialog1.ShowDialog ( ) == DialogResult.OK )
+			{
+				// Get both filenames.
+				var allocationsFileName   = openFileDialog1.FileName;
+				var configurationFileName = Allocations.ConfigurationFileName ( allocationsFileName );
+
+				// Parse configuration file.
+				if ( configurationFileName == null )
+					AT1Configuration = new Configuration ( );
+				else
+				{
+					using ( var configurationWebClient = new WebClient ( ) )
+					using ( var configurationStream = configurationWebClient.OpenRead ( configurationFileName ) )
+					using ( var configurationFile = new StreamReader ( configurationStream ) )
+					{
+						Configuration.TryParse ( configurationFile, configurationFileName, out AT1Configuration,
+												 out var configurationErrors );
+					}
+				}
+
+				// Parse Allocations file.
+				using ( var allocationsFile = new StreamReader ( allocationsFileName ) )
+				{
+					Allocations.TryParse ( allocationsFile, allocationsFileName, AT1Configuration, out AT1Allocations,
+										   out var allocationsErrors );
+				}
+
+				// Refesh GUI and Log errors.
+				UpdateGUI ( );
+				AT1Allocations.LogFileErrors ( AT1Allocations.FileErrorsTXT );
+				AT1Allocations.LogFileErrors ( AT1Configuration.FileErrorsTXT );
+			}
+		}
+
+		private void ExitToolStripMenuItem_Click ( object sender, EventArgs e )
+		{
+			this.Close ( );
+		}
+
+#endregion
+
+#region Clear and Update GUI
+
+		private void ClearGUI ( )
+		{
+			// As we are opening a Configuration file,
+			// indicate allocations are not valid, and clear GUI.
+			allocationToolStripMenuItem.Enabled = false;
+
+			if ( allocationsWebBrowser.Document != null )
+				allocationsWebBrowser.Document.OpenNew ( true );
+			allocationsWebBrowser.DocumentText = String.Empty;
+
+			if ( ErrorListViewer.WebBrowser.Document != null )
+				ErrorListViewer.WebBrowser.Document.OpenNew ( true );
+			ErrorListViewer.WebBrowser.DocumentText = String.Empty;
+		}
+
+		private void UpdateGUI ( )
+		{
+			// Update GUI:
+			// - enable menu
+			// - display Allocations data (whether valid or invalid)
+			// - Allocations and Configuration file errors.
+			if ( AT1Allocations.FileValid && AT1Configuration.FileValid )
+				allocationToolStripMenuItem.Enabled = true;
+
+			allocationsWebBrowser.DocumentText = AT1Allocations.ToStringHTML ( );
+
+			ErrorListViewer.WebBrowser.DocumentText =
+				AT1Allocations.FileErrorsHTML +
+				AT1Configuration.FileErrorsHTML +
+				AT1Allocations.AllocationsErrorsHTML;
+		}
+
+#endregion
+
+#region Validate menu event handlers
+
+		private void AllocationToolStripMenuItem_Click ( object sender, EventArgs e )
+		{
+			// Check if the allocations are valid.
+			AT1Allocations.Validate ( );
+
+			// Refesh GUI and Log errors.
+			UpdateGUI ( );
+			AT1Allocations.LogFileErrors ( AT1Allocations.AllocationsErrorsTXT );
+		}
+
+#endregion
+
+#region View menu event handlers
+
+		private void ErrorListToolStripMenuItem_Click ( object sender, EventArgs e )
+		{
+			if ( ErrorListViewer != null )
+			{
+				ErrorListViewer.WindowState = FormWindowState.Normal;
+				ErrorListViewer.Show ( );
+				ErrorListViewer.Activate ( );
+			}
+		}
+
+#endregion
+
+#region Help menu event handlers
+
+		private void AboutToolStripMenuItem_Click ( object sender, EventArgs e )
+		{
+			AboutBox.ShowDialog ( );
+		}
+
+#endregion
+
+#region Generate Allocations
+
+		private void generateAllocationsButton_Click ( object sender, EventArgs e )
+		{
+			var url = urlComboBox.Text;
+
+			var filename =
+				url.Substring ( url.LastIndexOf ( '/' ) + 1 ); // everything after the last "/" is the filename
+			using ( var configurationWebClient = new WebClient ( ) )
+			using ( var configurationStream = configurationWebClient.OpenRead ( url ) )
+			using ( var configurationFile = new StreamReader ( configurationStream ) )
+			{
+				Configuration.TryParse ( configurationFile,
+										 filename,
+										 out AT1Configuration,
+										 out var configurationErrors );
+			}
+
+
+			var localWcf = new ServiceReference1.ServiceClient ( );
+			var taff     = localWcf.Solve ( url );
+			Allocations.TryParse ( taff, AT1Configuration, out AT1Allocations, out var allocationErrors );
+
+			UpdateGUI ( );
+		}
+
+#endregion
+	}
+}
